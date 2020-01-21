7b1197bfc5f6f66ce1423d93b1e5379c063f0853
Oscar <osrofer00@gmail.com>
2020-01-21 12:09:55 +0100

    iconos editar borrar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,32 @@
          {{session('successMsg')}}
     </div>
 @endif
+<table class="table">
+  <thead class="black white-text">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellidos</th>
+      <th scope="col">Email</th>
+      <th scope="col">Teléfono</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($estudiantes as $estudiante)
+    <tr>
+      <th scope="row">{{$estudiante->id}}</th>
+      <td>{{$estudiante->nombre}}</td>
+      <td>{{$estudiante->apellidos}}</td>
+      <td>{{$estudiante->email}}</td>
+      <td>{{$estudiante->telefono}}</td>
+      <td>
+        <a class="btn btn-raised btn-primary bt-sm" href=""><i class="far fa-edit"></i></a>
+        <a class="btn btn-raised btn-danger bt-sm" href=""><i class="far fa-trash-alt"></i></a>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
 
 @endsection
